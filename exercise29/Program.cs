d278e702864dfe14be146d18ec7002e521809d1a
mikhail_malkov <mikhailmalkov9@gmail.com>
2022-11-25 11:24:14 -0500

    commited exercise29

---
+++ exercise29/Program.cs
@@ -0,0 +1,26 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int [] array = new int [8];
+PrintArray(array);
+Console.WriteLine(array);
+var newArray = FeelArray(array);
+PrintArray(newArray);
+int[]FeelArray( int [] arr)
+{
+    var result = new int [arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        result[i] = new Random().Next(0, 2);
+    }
+    PrintArray(result);
+    return result;
+}
+void PrintArray(int[]arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+}
